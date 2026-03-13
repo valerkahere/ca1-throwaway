3c0db4abd1f9e372b5f00de874f419908abaabb5
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-13 17:00:22 +0000

    display search and details components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Search } from './components/search/search';
+import { Details } from './components/details/details';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Search, Details],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
