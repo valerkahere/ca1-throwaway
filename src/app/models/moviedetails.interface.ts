959c2f120145420ca663f80e313f63d1e4998f46
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-13 17:05:45 +0000

    create interface

---
+++ src/app/models/moviedetails.interface.ts
@@ -0,0 +1,8 @@
+// fields you plan to display for your movie
+// title, year, director, poster - must have at least these properties
+interface MovieDetails {
+    Title: string;
+    Year: string;
+    Director: string;
+    Poster: string;
+}
