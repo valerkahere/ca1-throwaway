dbe9d259ffb7dd194f001fa895b282d1ba682ca7
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-23 00:24:02 +0000

    Changes: - Improve styling. - Add more info on about route - fix pagination, move to home route only - add copyright to header

--- src/app/routes/about/about.ts
+++ src/app/routes/about/about.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    RouterLinkWithHref,
+    RouterLinkActive
+  ],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
